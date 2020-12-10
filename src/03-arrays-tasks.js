b77898967dedf0d8139a4ee88a7405ea2cd630f4
anatkig <70736455+anatkig@users.noreply.github.com>
2020-12-10 11:31:38 +0600

    1 task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -165,8 +165,8 @@ function insertItem(arr, item, index) {
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
 
@@ -180,8 +180,8 @@ function getHead(/* arr, n */) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(arr.length - n);
 }
 
 
@@ -205,8 +205,19 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  let str = arr.reduce((sum, a) => {
+    let sumf = sum;
+    a.reduce((s, b) => {
+      let sf = s;
+      sf += b;
+      return sf;
+    }, '');
+    sumf += `${a}\n`;
+    return sumf;
+  }, '');
+  str = str.slice(0, str.length - 1);
+  return str;
 }
 
 /**
